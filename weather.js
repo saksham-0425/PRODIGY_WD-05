8dbbbb44ade70989f2b7c40b6dcc86bb54e69000
Saksham Gupta <guptasaksham2510@gmail.com>
2024-06-30 22:09:11 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,41 @@
+document.getElementById('location-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let locationInput = document.getElementById('location-input').value.trim();
+    
+    fetchWeather(locationInput);
+});
+
+async function fetchWeather(location) {
+    const apiKey = '67418b0a82213b8fa2d6722574dce838'; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const weatherData = await response.json();
+
+        if (weatherData.cod === '404') {
+            displayErrorMessage('Location not found. Please try again.');
+        } else {
+            displayWeatherInfo(weatherData);
+        }
+    } catch (error) {
+        displayErrorMessage('There was a problem fetching weather data. Please try again later.');
+    }
+}
+
+function displayWeatherInfo(data) {
+    const weatherInfoContainer = document.getElementById('weather-info');
+    weatherInfoContainer.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>${new Date().toDateString()}</p>
+        <p>Weather: ${data.weather[0].main}</p>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+}
+
+function displayErrorMessage(message) {
+    const weatherInfoContainer = document.getElementById('weather-info');
+    weatherInfoContainer.innerHTML = `<p>${message}</p>`;
+}
